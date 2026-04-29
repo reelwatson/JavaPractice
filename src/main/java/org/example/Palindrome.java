0af68ee9b414e7461969b6edba6591991387b0af
reelwatson <162487089+reelwatson@users.noreply.github.com>
2026-04-29 17:00:07 -0400

    Added Palindrome class and methods

--- src/main/java/org/example/Palindrome.java
+++ src/main/java/org/example/Palindrome.java
@@ -1,4 +1,23 @@
 package org.example;
 
 public class Palindrome {
+    public static void main(String[] args) {
+        Palindrome newPal = new Palindrome();
+        newPal.isPalindrome("Odysseus");
+    }
+    public boolean isPalindrome(String originalString) {
+        boolean isPalindrome = false;
+
+        StringBuilder firstString = new StringBuilder();
+        firstString.append(originalString);
+        StringBuilder reversedString = new StringBuilder();
+        reversedString.append(firstString);
+        reversedString.reverse();
+
+        if (reversedString.compareTo(firstString) == 0) {
+            isPalindrome = true;
+        }
+
+        return isPalindrome;
+    }
 }
