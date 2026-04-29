f593208694188455ad6d7ebf96ca27cf94c0ef85
reelwatson <162487089+reelwatson@users.noreply.github.com>
2026-04-29 17:03:03 -0400

    Created test class for Palindrome class

---
+++ src/test/java/org/example/PalindromeTest.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PalindromeTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void isPalindrome() {
+    }
+}
