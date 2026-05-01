0cfd3e47d7de78b954bd31b14ba46f62e2732c97
reelwatson <162487089+reelwatson@users.noreply.github.com>
2026-05-01 14:28:08 -0400

    Added request for name and age inputs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your name here: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt(); //will fail if float or String entered
+
+        System.out.println("Hello " + name);
+        System.out.println("You are " + age + " years old");
+
         scanner.close();
     }
 }
