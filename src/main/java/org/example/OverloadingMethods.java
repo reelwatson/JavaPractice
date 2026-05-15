cdf69283f4422fd15da4477ba6acf14fbe060066
reelwatson <162487089+reelwatson@users.noreply.github.com>
2026-05-15 08:31:47 -0400

    Updated overloaded method

--- src/main/java/org/example/OverloadingMethods.java
+++ src/main/java/org/example/OverloadingMethods.java
@@ -3,7 +3,8 @@ package org.example;
 public class OverloadingMethods {
     public static void main(String[] args) throws InterruptedException {
         litanyOfFear(3);
-        earthseedMantra(2);
+        earthseedMantra(3, 1);
+        earthseedMantra(1);
     }
 
     static void litanyOfFear(int x) throws InterruptedException {
@@ -18,5 +19,17 @@ public class OverloadingMethods {
             System.out.println("All that you touch you change.\nAll that you change changes you.\n");
             Thread.sleep(3000);
         }
+    }
+
+    static void earthseedMantra(int x, int y) throws InterruptedException {
+        for (int i = 0; i < x; i++) {
+            System.out.println("All that you touch you change.\nAll that you change changes you.\n");
+            Thread.sleep(2000);
+        }
+        for (int i = 0; i < y; i++) {
+            System.out.println("The only lasting truth is change.\nGod is change.\n");
+            Thread.sleep(2000);
+        }
+
     }
 }
