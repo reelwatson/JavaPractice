60dcd1fe285279f19a109b9d0b0de61523d2a7ee
reelwatson <162487089+reelwatson@users.noreply.github.com>
2026-06-19 21:20:16 -0400

    Quick update

---
+++ src/main/java/org/example/ByteArrayInputStream.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.io.InputStream;
+
+public class ByteArrayInputStream {
+
+    public static void main(String[] args) {
+        byte[] byteArray = {1, 2, 4, 8, 16};
+    }
+
+
+}
